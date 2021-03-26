954e4d367d7d784e860cc161b4bd567525710b38
Anuj Garg <Anuj.Garg@XEBIAINDIA.LOCAL>
2021-03-26 13:07:57 +0530

    Removed default code andded codecovrage flag

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('login-test-fe');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('login-test-fe app is running!');
-  });
 });
